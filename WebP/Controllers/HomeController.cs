54f9d1b81817935656c6c41c9ab068bcc2082a2f
DESKTOP-5PB7OBT\User <kolya-kirich@i.ua>
2023-10-30 18:49:07 +0200

    added new page and new buttons on the page which adress to item page

--- WebP/Controllers/HomeController.cs
+++ WebP/Controllers/HomeController.cs
@@ -28,6 +28,10 @@ namespace WebP.Controllers
         {
             return View();
         }
+        public IActionResult ListOfItems()
+        {
+            return View();
+        }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
